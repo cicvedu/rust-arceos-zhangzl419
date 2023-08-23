db6a97db78e5d38b784dea99830b6e68a12c640c
root <root@zj.localdomain>
2023-08-23 11:26:15 +0800

    修改代码细节

--- exercises/cos_arceos/crates/allocator/src/simple.rs
+++ exercises/cos_arceos/crates/allocator/src/simple.rs
@@ -27,45 +27,15 @@ impl SimpleByteAllocator {
     }
 }
 
-// impl BaseAllocator for SimpleByteAllocator {
-//     fn init(&mut self, _start: usize, _size: usize) {
-//         todo!();
-//     }
-
-//     fn add_memory(&mut self, _start: usize, _size: usize) -> AllocResult {
-//         todo!();
-//     }
-// }
-
 impl BaseAllocator for SimpleByteAllocator {
-    fn init(&mut self, _start: usize, _size: usize) {}
+    fn init(&mut self, _start: usize, _size: usize) {
+    }
 
     fn add_memory(&mut self, _start: usize, _size: usize) -> AllocResult {
         Ok(())
     }
 }
 
-// impl ByteAllocator for SimpleByteAllocator {
-//     fn alloc(&mut self, _layout: Layout) -> AllocResult<NonZeroUsize> {
-//         todo!();
-//     }
-
-//     fn dealloc(&mut self, _pos: NonZeroUsize, _layout: Layout) {
-//         todo!();
-//     }
-
-//     fn total_bytes(&self) -> usize {
-//         todo!();
-//     }
-
-//     fn used_bytes(&self) -> usize {
-//         todo!();
-//     }
-
-//     fn available_bytes(&self) -> usize {
-//         todo!();
-//     }
-// }
 impl ByteAllocator for SimpleByteAllocator {
     fn alloc(&mut self, layout: Layout) -> AllocResult<NonZeroUsize> {
         let size = layout.size();
