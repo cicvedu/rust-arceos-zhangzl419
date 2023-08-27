7e39d3838c9acea9c6c83acecc33c01966aacb83
root <root@zj.localdomain>
2023-08-27 22:20:54 +0800

    a2

--- exercises/cos_arceos/crates/scheduler/src/simple.rs
+++ exercises/cos_arceos/crates/scheduler/src/simple.rs
@@ -1,17 +1,29 @@
 use alloc::{collections::VecDeque, sync::Arc};
-use core::ops::Deref;
+use core::{ops::Deref, sync::atomic::AtomicIsize, sync::atomic::Ordering};
 
 use crate::BaseScheduler;
 
 /// A task wrapper for the [`SimpleScheduler`].
 pub struct SimpleTask<T> {
     inner: T,
+    time_slice: AtomicIsize,
 }
 
 impl<T> SimpleTask<T> {
     /// Creates a new [`SimpleTask`] from the inner task struct.
     pub const fn new(inner: T) -> Self {
-        Self { inner }
+        Self { 
+            inner,
+            time_slice: AtomicIsize::new(5), 
+        }
+    }
+
+    fn time_slice(&self) -> isize {
+        self.time_slice.load(Ordering::Acquire)
+    }
+
+    fn reset_time_slice(&self) {
+        self.time_slice.store(5, Ordering::Release);
     }
 
     /// Returns a reference to the inner task struct.
@@ -75,15 +87,56 @@ impl<T> BaseScheduler for SimpleScheduler<T> {
         self.ready_queue.pop_front()
     }
 
-    fn put_prev_task(&mut self, prev: Self::SchedItem, _preempt: bool) {
-        self.ready_queue.push_back(prev);
+    fn put_prev_task(&mut self, prev: Self::SchedItem, preempt: bool) {
+        if prev.time_slice() > 0 && preempt {
+            self.ready_queue.push_front(prev)
+        } else {
+            prev.reset_time_slice();
+            self.ready_queue.push_back(prev);
+        }
     }
 
-    fn task_tick(&mut self, _current: &Self::SchedItem) -> bool {
-        false // no reschedule
+    fn task_tick(&mut self, current: &Self::SchedItem) -> bool {
+        let old_slice = current.time_slice.fetch_sub(1, core::sync::atomic::Ordering::Release);
+        old_slice <= 1 // no reschedule
     }
 
     fn set_priority(&mut self, _task: &Self::SchedItem, _prio: isize) -> bool {
         false
     }
 }
+
+// impl<T> BaseScheduler for SimpleScheduler<T> {
+//     type SchedItem = Arc<SimpleTask<T>>;
+
+//     fn init(&mut self) {}
+
+//     fn add_task(&mut self, task: Self::SchedItem) {
+//         trace!("######### add_task");
+//         self.ready_queue.push_back(task);
+//     }
+
+//     fn remove_task(&mut self, task: &Self::SchedItem) -> Option<Self::SchedItem> {
+//         trace!("######### remove_task");
+//         self.ready_queue
+//             .iter()
+//             .position(|t| Arc::ptr_eq(t, task))
+//             .and_then(|idx| self.ready_queue.remove(idx))
+//     }
+
+//     fn pick_next_task(&mut self) -> Option<Self::SchedItem> {
+//         self.ready_queue.pop_front()
+//     }
+
+//     fn put_prev_task(&mut self, prev: Self::SchedItem, _preempt: bool) {
+//         self.ready_queue.push_back(prev);
+//     }
+
+//     fn task_tick(&mut self, _current: &Self::SchedItem) -> bool {
+//         false // no reschedule
+//     }
+
+//     fn set_priority(&mut self, _task: &Self::SchedItem, _prio: isize) -> bool {
+//         false
+//     }
+// }
